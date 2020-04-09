03016a07cf5e3369ce56738b8df8b47409a93e96
rbezghin <radomirbezgin@gmail.com>
2020-04-08 17:09:18 -0700

    small rotation fix

--- UCSCBus/Bus.swift
+++ UCSCBus/Bus.swift
@@ -25,6 +25,9 @@ struct Bus: Equatable, CustomStringConvertible {
     var sourceIdentifier: String
     var busLayerIdentifier: String
     var busImageName: String
+    // TODO:  (Radomyr) may need to calibrate this
+    //used in func busDidntMove TODO
+    let negligibleChange = 0.00001
 
     init(id: Int, busType: String, coordinate: CLLocationCoordinate2D) {
         self.id = id
@@ -47,7 +50,12 @@ struct Bus: Equatable, CustomStringConvertible {
         oldCoordinate = coordinate
         coordinate = newCoordinate
     }
+    
+    //get how many degrees icon needs to be rotated
     func getBearing() -> Double {
+        if(busDidntMove()){
+            return 0 //dont rotate
+        }
         let x1 = oldCoordinate.longitude * (Double.pi / 180.0)
         let y1 = oldCoordinate.latitude  * (Double.pi / 180.0)
         let x2 = coordinate.longitude   * (Double.pi / 180.0)
@@ -56,4 +64,39 @@ struct Bus: Equatable, CustomStringConvertible {
         let sita = atan2(sin(dx) * cos(y2), cos(y1) * sin(y2) - sin(y1) * cos(y2) * cos(dx))
         return ((sita * (180.0 / Double.pi)))
     }
+    
+    // need to check how much bus moved to avoid unneccessary rotation when bus is at the same spot
+    func busDidntMove()-> Bool{
+        if(coordinate.latitude == oldCoordinate.latitude && coordinate.longitude == oldCoordinate.longitude){
+            return true
+        }
+        let dy = abs(coordinate.latitude - oldCoordinate.latitude)
+        let dx = abs(coordinate.longitude - oldCoordinate.longitude)
+        if (dy < negligibleChange && dx < negligibleChange){
+            return true
+        }
+        return false
+    }
 }
+
+
+//
+//{
+//    basestation = OPERS;
+//    id = 93;
+//    lat = "36.99150167";
+//    lon = "-122.05488833";
+//    "time_stamp" = "2020-04-08 16:37:50";
+//    type = LOOP;
+//}
+//{
+//    basestation = OPERS;
+//    id = 93;
+//    lat = "36.99211";
+//    lon = "-122.05517333";
+//    "time_stamp" = "2020-04-08 16:37:56";
+//    type = LOOP;
+//
+//  36.99150167 - 36.99211 =  -0.00060833
+//  -122.05488833 + 122.05517333 = 0.000285
+//}
