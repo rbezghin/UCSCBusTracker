b7bd7666f1a968b04b8f03b58b84d3c00d40d7d7
rbezghin <radomirbezgin@gmail.com>
2020-06-02 12:09:32 -0700

    restructured files + custom cell

---
+++ UCSCBus/ScheduleTableViewController/BusETATableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  BusETATableViewCell.swift
+//  UCSCBus
+//
+//  Created by Radomyr Bezghin on 6/2/20.
+//  Copyright © 2020 Radomyr Bezghin. All rights reserved.
+//
+
+import UIKit
+
+class BusETATableViewCell: UITableViewCell {
+    
+    let busIconImage: UIImage
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
